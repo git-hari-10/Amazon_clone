0ee35ad7c544ed51904518a73c53973b20ca486c
Harish <harishvigneshcoc@gmail.com>
2025-08-09 15:33:39 +0530

    replaced promises and callbacks using async & await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import {loadProducts, loadProductsFetch} from "../data/products.js";
+import { loadProductsFetch } from "../data/products.js";
 
 async function initCheckout() {
     await loadProductsFetch();
